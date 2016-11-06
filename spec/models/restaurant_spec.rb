fd91b696015483c235eb87ff368ee643671a002d
Conrad Thompson <conrad-thompson@uiowa.edu>
2016-11-06 19:08:49 +0000

    Added tests to restaurant_spec.rb

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'adding restaurant' do
+    it 'should create a session token' do
+       expect(SecureRandom).to receive(:base64).and_return('012345678901234567890123')
+       restaurant = Restaurant.create_restaurant!(name: 'Test', email: 'fake_email')
+       expect(restaurant).to have_attributes(:session_token => '012345678901234567890123')
+    end
+    it 'should call Restaurant create' do
+        fake_params = {fake1: double('param1'), fake2: double('param2')}
+        expect(SecureRandom).to receive(:base64)
+        expect(Restaurant).to receive(:create!)
+        Restaurant.create_restaurant!(fake_params)
+    end
+    it 'should be valid with valid attributes' do
+        restaurant = Restaurant.new(name: 'Test', email: 'fake_email')
+        expect(restaurant).to be_valid
+    end
+    it 'should be invalid without name' do
+        restaurant = Restaurant.new(name: nil)
+        expect(restaurant).to_not be_valid
+    end
+    it 'should be invalid without email' do
+        restaurant = Restaurant.new(email: nil)
+        expect(restaurant).to_not be_valid
+    end
+  end
 end
