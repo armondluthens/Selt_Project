e2ba6cac346da5c90efa2a5bfc44fa46fed9a25f
Armond Luthens <armondluthens@gmail.com>
2016-11-15 02:08:26 +0000

    changed login, still need to figure out encrypted password matching

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,13 @@ class SessionsController < ApplicationController
     # default: render 'new' template
   end
   
+
   def create
     #puts session_params
     #@restaurant = Restaurant.find_by(name: session_params[:name], password: session_params[:password])
     if (Restaurant.exists? email: session_params['email'])
       restaurant = Restaurant.find_by_email(session_params['email'])
-      if(restaurant.email == session_params['email'])
+      if(restaurant.password == session_params['password'])
         session[:session_token] = restaurant.session_token
         flash[:notice] = "You are logged in as #{restaurant.name}."
         redirect_to root_path
@@ -25,9 +26,11 @@ class SessionsController < ApplicationController
     
   end
 
+
   def destroy
     reset_session
     redirect_to root_path
   end
     
 end
+
