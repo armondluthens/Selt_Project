73d21d6f4765bf4d9238c8bcf6cf13dbea0d7934
Cindy <cindy-ta@uiowa.edu>
2016-11-18 07:59:01 +0000

    Added start and end date checks for deals. Needs to prevent form from submitting, not throw error

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -5,10 +5,23 @@ class Deal < ActiveRecord::Base
   
   validates :title, presence: true
   
+  validates_presence_of :start_date, :end_date
+
+  validate :end_date_is_after_start_date
+
+  
   
   def self.create_deal!(params)
         #params[:session_token] = SecureRandom.base64 #don't think a deal needs a session token
         Deal.create!(params)
+  end
+  
+  def end_date_is_after_start_date
+    return if end_date.blank? || start_date.blank?
+  
+    if end_date < start_date
+      errors.add(:end_date, "cannot be before the start date") 
+    end 
   end
   
   
