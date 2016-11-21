8fc4b813ac9227e92afb1882bcccaf93e4d202a7
Cindy <cindy-ta@uiowa.edu>
2016-11-21 05:14:26 +0000

    yay db queries aren't erroring out anymore

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,15 @@
 class Search < ActiveRecord::Base
+    
+  def self.search_deals!(params)
+      
+    deals = Deal.all
+    
+    deals = deals.where(["title LIKE ?", "%#{params[:title]}%"]) if params[:title].present?
+    deals = deals.where(["ethnicity LIKE ?", params[:ethnicity]]) if params[:ethnicity].present?
+    deals = deals.where(["restaurant_name LIKE ?", "%#{params[:restaurant_name]}%"]) if params[:restaurant_name].present?
+    
+    Deal.create!(params)
+  end
+  
+  
 end
