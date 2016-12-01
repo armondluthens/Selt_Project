f07bc4c3ee020508a1e9faa47f1fee1152fc534c
Jackson Theisen <jackson-theisen@uiowa.edu>
2016-12-01 21:46:20 +0000

    added latitude/longitude columns to restaurants table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121022236) do
+ActiveRecord::Schema.define(version: 20161201213841) do
 
   create_table "deals", force: :cascade do |t|
     t.string   "title"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20161121022236) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "searches", force: :cascade do |t|
