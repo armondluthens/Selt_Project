c734f418116b3b288ca2e86120f1801bd4552c5b
Armond Luthens <armondluthens@gmail.com>
2016-12-09 19:58:53 +0000

    back

--- app/models/session.rb
+++ app/models/session.rb
@@ -3,7 +3,8 @@ class Deal < ActiveRecord::Base
   
   
   def self.search(term)
-    if Deal.sunday == true && Time.now.in_time_zone("Central Time (US & Canada)").sunday?
+    #if Deal.sunday == true && Time.now.in_time_zone("Central Time (US & Canada)").sunday?
+    if Time.now.in_time_zone("Central Time (US & Canada)").sunday?
       where("title like :term or description like :term or restaurant_name like :term", term: "%#{term}%")
     elsif Deal.monday == true && Time.now.in_time_zone("Central Time (US & Canada)").monday?
       where("title like :term or description like :term or restaurant_name like :term", term: "%#{term}%")
